85b6d4e6dbf416decd11b64d1f63efd167ea9072
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-11-27 15:44:03 -0500

    ascending order

--- controllers/bands_controllers.js
+++ controllers/bands_controllers.js
@@ -5,12 +5,14 @@ const { Band } = db
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [[ 'available_start_time', 'ASC']]
+        })
         res.status(200).json(foundBands)
-    } catch (error) {
+     } catch (error) {
         res.status(500).json(error)
     }
-})
+});
 // FIND A SPECIFIC BAND
 bands.get('/:id', async (req, res) => {
     try {
