8e595ee90ce39ea93507f627d440c79e7d7bc420
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-11-27 16:22:42 -0500

    added a queryto Filter by Band Name

--- controllers/bands_controllers.js
+++ controllers/bands_controllers.js
@@ -1,12 +1,16 @@
 const bands =require('express').Router()
 const db = require('../models')
 const { Band } = db
+const { Op } = require('sequelize')
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
         const foundBands = await Band.findAll({
-            order: [[ 'available_start_time', 'ASC']]
+            order: [[ 'available_start_time', 'ASC']],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name: ''}%` }
+            }
         })
         res.status(200).json(foundBands)
      } catch (error) {
@@ -66,8 +70,6 @@ bands.delete('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
-
-
 
 
 
