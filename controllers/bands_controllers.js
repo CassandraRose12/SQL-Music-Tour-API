a7c0171cfa747c0d8b098fad5413e1eef8d6144d
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-11-27 15:17:24 -0500

    create band route

--- controllers/bands_controllers.js
+++ controllers/bands_controllers.js
@@ -22,6 +22,19 @@ bands.get('/:id', async (req, res) => {
         res.status(500).json(error)
     }
 })
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 
 
 
